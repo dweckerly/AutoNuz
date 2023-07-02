8d1a45d207b8f3247451d11466d4667748dcfe6d
David <d.weckerly@gmail.com>
2023-07-01 20:36:51 -0500

    critter selection

--- Assets/Scripts/MapMaker.cs
+++ Assets/Scripts/MapMaker.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.Linq;
 
 [System.Serializable]
 public class MapItem
 {
     public Image locationSprites;
     public TMP_Text locationName;
+    public LocationData location;
 }
 
 public class MapMaker : MonoBehaviour
@@ -41,11 +41,60 @@ public class MapMaker : MonoBehaviour
         {
             MapItems[i].locationSprites.sprite = selectedLocations[i].LocationSprite;
             MapItems[i].locationName.text = selectedLocations[i].LocationName;
+            MapItems[i].location = selectedLocations[i];
         }
     }
 
     public int SelectMapItem(List<int> ids)
     {
         return ids[Random.Range(0, ids.Count)];
+    }
+
+    public CritterData SelectAreaToExplore(int index)
+    {
+        float rate = Random.Range(0, 1);
+        if (rate >= 1f)
+        {
+            if (CatchTableHasRateValue(index, CatchRate.UltraRare))
+                return FindCritterByCatchRate(index, CatchRate.UltraRare);
+        }
+        if (rate >= 0.9f)
+        {
+            if (CatchTableHasRateValue(index, CatchRate.Rare))
+                return FindCritterByCatchRate(index, CatchRate.Rare);
+        }
+        if (rate >= 0.6f)
+        {
+            if (CatchTableHasRateValue(index, CatchRate.Uncommon))
+                return FindCritterByCatchRate(index, CatchRate.Uncommon);
+        }
+        return FindCritterByCatchRate(index, CatchRate.Common);
+    }
+
+    bool CatchTableHasRateValue(int mapIndex, CatchRate catchRate)
+    {
+        for (int i = 0; i < MapItems[mapIndex].location.CatchTable.Length; i++)
+        {
+            if (MapItems[mapIndex].location.CatchTable[i].CatchRate == catchRate)
+                return true;
+        }
+        return false;
+    }
+
+    public CritterData FindCritterByCatchRate(int mapIndex, CatchRate catchRate)
+    {
+        List<CritterData> possibleCritters = new List<CritterData>();
+        for (int i = 0; i < MapItems[mapIndex].location.CatchTable.Length; i++)
+        {
+            if (MapItems[mapIndex].location.CatchTable[i].CatchRate == catchRate)
+            {
+                foreach (CritterData c in MapItems[mapIndex].location.CatchTable[i].Critters)
+                {
+                    possibleCritters.Add(c);
+                }
+            }  
+        }
+        int selectedCritterIndex = Random.Range(0, possibleCritters.Count);
+        return possibleCritters[selectedCritterIndex];
     }
 }
