cfa9f03584e72110aefce6166bb2e02029a3007e
David <d.weckerly@gmail.com>
2023-07-03 10:08:22 -0500

    initial critter object stubbing

--- Assets/Scripts/Data/Critters/CritterData.cs
+++ Assets/Scripts/Data/Critters/CritterData.cs
@@ -2,9 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum StatProgressionRate
+{
+    Poor,
+    Fair,
+    Average,
+    Superior,
+    Exemplary
+}
+
+public enum LevelProgressionRate
+{
+    Slow,
+    Medium,
+    Fast
+}
+
 [CreateAssetMenu(fileName = "critter", menuName = "Data/Critter", order = 1)]
 public class CritterData : ScriptableObject
 {
     public string CritterName;
     public Sprite CritterSprite;
+    public LevelProgressionRate levelingRate;
+    public StatProgressionRate HpProgressionRate;
+    public StatProgressionRate AttackProgressionRate;
+    public StatProgressionRate DefenseProgressionRate;
+    public StatProgressionRate SpeedProgressionRate;
 }
