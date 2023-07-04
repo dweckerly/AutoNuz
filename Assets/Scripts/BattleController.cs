423365cbf0ab64ab33051115d331fc54741bce7a
David <d.weckerly@gmail.com>
2023-07-03 20:16:05 -0500

    added level text in battle ui

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -12,6 +12,7 @@ public class CritterBattleUI
     public RectTransform healthRect;
     public RectTransform speedRect;
     public TMP_Text healthText;
+    public TMP_Text levelText;
 }
 
 public class BattleController : MonoBehaviour
@@ -28,16 +29,18 @@ public class BattleController : MonoBehaviour
         playerCritterUI.healthText.text = playerCritter.currentHp + "/" + playerCritter.Hp;
         playerCritterUI.healthRect.localScale = new Vector3(CalculateHealthPercentage(playerCritter), 1f, 1f);
         playerCritterUI.speedRect.localScale = new Vector3(0, 1f, 1f);
+        playerCritterUI.levelText.text = "LVL: " + playerCritter.Level;
 
         wildCritterUI.critterSprite.sprite = wildCritter.data.CritterSprite;
         wildCritterUI.healthText.text = wildCritter.currentHp + "/" + wildCritter.Hp;
         wildCritterUI.healthRect.localScale = new Vector3(CalculateHealthPercentage(wildCritter), 1f, 1f);
         wildCritterUI.speedRect.localScale = new Vector3(0, 1f, 1f);
+        wildCritterUI.levelText.text = "LVL: " + wildCritter.Level;
     }
 
     public void StartBattle()
     {
-        
+
     }
 
     public void RunFromFight()
