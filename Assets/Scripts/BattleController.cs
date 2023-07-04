601f9cec6817d914431ef7f9cd4a18bc5f6a3576
dweckerly <d.weckerly@gmail.com>
2023-07-04 11:05:25 -0500

    caching critter objects for battle

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -23,8 +23,13 @@ public class BattleController : MonoBehaviour
 
     public event Action OnRunSelected;
 
-    public void PopulateBattleUI(Critter playerCritter, Critter wildCritter)
+    Critter playerCritter;
+    Critter wildCritter;
+
+    public void PopulateBattleUI(Critter _playerCritter, Critter _wildCritter)
     {
+        playerCritter = _playerCritter;
+        wildCritter = _wildCritter;
         playerCritterUI.critterSprite.sprite = playerCritter.data.CritterSprite;
         playerCritterUI.healthText.text = playerCritter.currentHp + "/" + playerCritter.Hp;
         playerCritterUI.healthRect.localScale = new Vector3(CalculateHealthPercentage(playerCritter), 1f, 1f);
@@ -40,7 +45,7 @@ public class BattleController : MonoBehaviour
 
     public void StartBattle()
     {
-
+        
     }
 
     public void RunFromFight()
