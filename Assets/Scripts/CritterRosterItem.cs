82a6e14ed916fcad9c68c96b4bc2e34669c24d52
dweckerly <d.weckerly@gmail.com>
2023-07-07 12:12:26 -0500

    working on critter roster display

---
+++ Assets/Scripts/CritterRosterItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CritterRosterItem : MonoBehaviour
+{
+    public Image image;
+    public TMP_Text level;
+    public RectTransform healthBar;
+    public TMP_Text healthText;
+}
